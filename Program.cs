a7c2832d6fffdca2b08638f2c81d7da56d9b10d7
Luca Spoldi <luca.spoldi@galileo.galileicrema.it>
2024-07-01 22:34:03 +0200

    Inizio progetto 1) commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using System;
+using System.Text;
+using static Raylib_cs.Raylib;
+
+namespace Examples.Core;
+
+public class BasicWindow
+{
+    public static int Main()
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+        SetTargetFPS(60);
+        //--------------------------------------------------------------------------------------
+
+        // Main game loop
+        while (!WindowShouldClose())
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            // TODO: Update your variables here
+            //----------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(Raylib_cs.Color.RayWhite);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, Raylib_cs.Color.Maroon);
+
+            EndDrawing();
+            //----------------------------------------------------------------------------------
+        }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        CloseWindow();
+        //--------------------------------------------------------------------------------------
+
+        return 0;
+    }
+}
